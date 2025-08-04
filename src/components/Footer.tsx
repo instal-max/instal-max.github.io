8649506125c2e4a5e5247850c27dbffe9dd7309e
instal-max <marcinlyczko@int.pl>
2025-08-04 14:46:16 +0200

    Make footer year dynamic instead of hardcoded 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -80,7 +80,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
         <div className="footer-bottom">
-          <p>&copy; 2025 InstalMax. Wszelkie prawa zastrzeżone.</p>
+          <p>&copy; {new Date().getFullYear()} InstalMax. Wszelkie prawa zastrzeżone.</p>
         </div>
       </div>
     </footer>
